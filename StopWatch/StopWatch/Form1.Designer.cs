d94de66b840019c86fce630706ba2f8ff471244b
Yann Boca <116502149+yannbocaa@users.noreply.github.com>
2023-11-12 20:23:56 +0100

    Add files via upload

---
+++ StopWatch/StopWatch/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace StopWatch
+{
+    partial class StopWatch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.watchLabel = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.formTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // watchLabel
+            // 
+            this.watchLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 90F, System.Drawing.FontStyle.Bold);
+            this.watchLabel.Location = new System.Drawing.Point(12, 104);
+            this.watchLabel.Name = "watchLabel";
+            this.watchLabel.Size = new System.Drawing.Size(987, 240);
+            this.watchLabel.TabIndex = 0;
+            this.watchLabel.Text = "00:00.00";
+            this.watchLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.Lime;
+            this.btnStart.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(12, 392);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(325, 148);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.Red;
+            this.btnStop.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(349, 392);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(325, 148);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "STOP";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.Yellow;
+            this.btnReset.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(688, 392);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(311, 148);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "RESET";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // formTimer
+            // 
+            this.formTimer.Interval = 10;
+            this.formTimer.Tick += new System.EventHandler(this.formTimer_Tick);
+            // 
+            // StopWatch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.ClientSize = new System.Drawing.Size(1011, 589);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.watchLabel);
+            this.Name = "StopWatch";
+            this.Text = "Stop Watch";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label watchLabel;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Timer formTimer;
+    }
+}
+
---
+++ StopWatch/StopWatch/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StopWatch
+{
+    public partial class StopWatch : Form
+    {
+        private DateTime startTime;
+        private bool isRunning = false;
+
+        public StopWatch()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // Set a value to start time
+            // startTime = DateTime.Now;
+
+            //Start the timer
+            //formTimer.Start();
+
+            if (!isRunning)
+            {
+                startTime = DateTime.Now;
+                formTimer.Start();
+                isRunning = true;
+            }
+            else 
+            {
+                startTime += DateTime.Now - startTime;
+                formTimer.Start();
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            formTimer.Stop();
+            isRunning = false;
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            formTimer.Stop();
+            watchLabel.Text = "00:00.00";
+            isRunning = false;
+        }
+
+        private void formTimer_Tick(object sender, EventArgs e)
+        {
+            // Calculate how long it's been since start
+            TimeSpan span = DateTime.Now - startTime;
+            watchLabel.Text = span.ToString(@"mm\:ss\.ff");  
+        }
+    }
+}
